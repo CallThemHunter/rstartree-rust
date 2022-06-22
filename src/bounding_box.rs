562923f1e929087674f83f9a342b10e07efd4f5e
hunter <ianrozen@buffalo.edu>
2022-06-21 21:40:09 -0400

    switched to borrowed instances.

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -1,75 +1,76 @@
+
 struct BoundingBox {
     lower_bound: Vec<f64>,
     upper_bound: Vec<f64>,
 }
 
 
-trait HasMargin<T, Other = Self> {
-    fn margin(self) -> T;
+trait HasMargin<T> {
+    fn margin(&self) -> T;
 
-    fn margin_diff(self, other: Other) -> T;
+    fn margin_diff(&self, other: &Self) -> T;
 }
 
-trait HasVolume<T, Other = Self> {
-    fn volume(self) -> T;
+trait HasVolume<T> {
+    fn volume(&self) -> T;
 
-    fn volume_diff(self, other: Other) -> T;
+    fn volume_diff(&self, other: &Self) -> T;
 }
 
-trait Overlap<T, Other = Self> {
-    fn overlap(self, other: Other) -> Self;
+trait Overlap<T> {
+    fn overlap(&self, other: &Self) -> Self;
 }
 
 trait Geometry<T> {
-    fn center(self) -> Vec<T>;
+    fn center(&self) -> Vec<T>;
 
-    fn center_along(self, dim: i32) -> T;
+    fn center_along(&self, dim: i32) -> T;
 
-    fn shape(self) -> Vec<T>;
+    fn shape(&self) -> Vec<T>;
 
-    fn width_of(self, dim: i32) -> T;
+    fn width_of(&self, dim: i32) -> T;
 }
 
 impl HasMargin<f64> for BoundingBox {
-    fn margin(self) -> f64 {
+    fn margin(&self) -> f64 {
         todo!()
     }
 
-    fn margin_diff(self, other: Self) -> f64 {
+    fn margin_diff(&self, other: &Self) -> f64 {
         todo!()
     }
 }
 
 impl HasVolume<f64> for BoundingBox {
-    fn volume(self) -> f64 {
+    fn volume(&self) -> f64 {
         todo!()
     }
 
-    fn volume_diff(self, other: Self) -> f64 {
+    fn volume_diff(&self, other: &Self) -> f64 {
         todo!()
     }
 }
 
 impl Overlap<f64> for BoundingBox {
-    fn overlap(self, other: Self) -> Self {
+    fn overlap(&self, other: &Self) -> Self {
         todo!()
     }
 }
 
 impl Geometry<f64> for BoundingBox {
-    fn center(self) -> Vec<f64> {
+    fn center(&self) -> Vec<f64> {
         todo!()
     }
 
-    fn center_along(self, dim: i32) -> f64 {
+    fn center_along(&self, dim: i32) -> f64 {
         todo!()
     }
 
-    fn shape(self) -> Vec<f64> {
+    fn shape(&self) -> Vec<f64> {
         todo!()
     }
 
-    fn width_of(self, dim: i32) -> f64 {
+    fn width_of(&self, dim: i32) -> f64 {
         todo!()
     }
 }
