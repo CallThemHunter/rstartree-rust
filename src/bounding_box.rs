5572010675dee5f3d4d394735d3f6b4bf70a4c2b
hunter <ianrozen@buffalo.edu>
2022-06-22 18:01:18 -0400

    implementing center along and width_of

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -1,7 +1,20 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::io::ErrorKind::InvalidData;
+
+//type Result<T> = std::result::Result<T, IndexOutOfBoundsError>;
+
+struct IndexOutOfBoundsError;
+
+impl Display for IndexOutOfBoundsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "incorrect index bounds")
+    }
+}
 
 struct BoundingBox {
-    lower_bound: Vec<f64>,
-    upper_bound: Vec<f64>,
+    lower_bounds: Vec<f64>,
+    upper_bounds: Vec<f64>,
 }
 
 trait HasMargin<T> {
@@ -23,11 +36,11 @@ trait Overlap<T>: Sized {
 trait Geometry<T> {
     fn center(&self) -> Vec<T>;
 
-    fn center_along(&self, dim: i32) -> T;
+    fn center_along(&self, dim: usize) -> Result<T, IndexOutOfBoundsError>;
 
     fn shape(&self) -> Vec<T>;
 
-    fn width_of(&self, dim: i32) -> T;
+    fn width_of(&self, dim: usize) -> Result<f64, IndexOutOfBoundsError>;
 }
 
 impl HasMargin<f64> for BoundingBox {
@@ -58,18 +71,47 @@ impl Overlap<f64> for BoundingBox {
 
 impl Geometry<f64> for BoundingBox {
     fn center(&self) -> Vec<f64> {
-        todo!()
+        let mut center: Vec<f64> = vec![];
+        for (lower, upper) in self.upper_bounds.iter().zip(self.upper_bounds.iter()) {
+            center.push((lower + upper) / 2.0)
+        };
+
+        center
     }
 
-    fn center_along(&self, dim: i32) -> f64 {
-        todo!()
+    fn center_along(&self, dim: usize) -> Result<f64, IndexOutOfBoundsError> {
+        let lower = self.lower_bounds.get(dim);
+        let lower = match lower {
+            None => return Err(IndexOutOfBoundsError),
+            Some(x) => x
+        };
+
+        let upper = self.upper_bounds.get(dim);
+        let upper = match upper {
+            None => return Err(IndexOutOfBoundsError),
+            Some(x) => x
+        };
+
+        Ok((upper + lower) / 2.0)
     }
 
     fn shape(&self) -> Vec<f64> {
         todo!()
     }
 
-    fn width_of(&self, dim: i32) -> f64 {
-        todo!()
+    fn width_of(&self, dim: usize) -> Result<f64, IndexOutOfBoundsError> {
+        let upper = self.upper_bounds.get(dim);
+        let upper = match upper {
+            None => return Err(IndexOutOfBoundsError),
+            Some(x) => x
+        };
+
+        let lower = self.lower_bounds.get(dim);
+        let lower = match lower {
+            None => return Err(IndexOutOfBoundsError),
+            Some(x) => x
+        };
+
+        Ok(upper - lower)
     }
 }
