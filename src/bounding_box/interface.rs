e5c3b10117d6dfe38a31d7159f35342e128c6e2b
hunter <ianrozen@buffalo.edu>
2022-06-28 22:24:56 -0400

    bounding box split into interface and implementations

---
+++ src/bounding_box/interface.rs
@@ -0,0 +1,44 @@
+use std::fmt::{self, Display, Formatter};
+
+pub struct BoundingBox<T> {
+    pub lower_bounds: Vec<T>,
+    pub upper_bounds: Vec<T>,
+}
+
+pub struct IndexOutOfBoundsError;
+
+impl Display for IndexOutOfBoundsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "incorrect index bounds")
+    }
+}
+
+pub trait HasMargin<T> {
+    fn margin(&self) -> T;
+
+    fn margin_diff(&self, other: &Self) -> T;
+}
+
+pub trait HasVolume<T> {
+    fn volume(&self) -> T;
+
+    fn volume_diff(&self, other: &Self) -> T;
+}
+
+pub trait Overlap<T>: Sized {
+    fn overlap(&self, other: &Self) -> Option<Self>;
+
+    fn encloses(&self, other: &Self) -> bool;
+}
+
+pub trait Geometry<T> {
+    fn center(&self) -> Vec<T>;
+
+    fn center_along(&self, dim: usize) -> Result<T, IndexOutOfBoundsError>;
+
+    fn shape(&self) -> Vec<T>;
+
+    fn width_of(&self, dim: usize) -> Result<f64, IndexOutOfBoundsError>;
+
+    fn asymmetry(&self, other: &Self, dim: usize) -> Result<f64, IndexOutOfBoundsError>;
+}
