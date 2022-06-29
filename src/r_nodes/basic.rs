cbe43e1fe0e74a4ac2f8cbad090e30b7573c317f
hunter <ianrozen@buffalo.edu>
2022-06-28 23:47:43 -0400

    made structs and traits public, added num_elements and num_nodes method

--- src/r_nodes/basic.rs
+++ src/r_nodes/basic.rs
@@ -1,14 +1,12 @@
-use std::process::Child;
-
 use crate::bounding_box::interface::BoundingBox;
 use crate::r_nodes::basic::Children::{Boxes, Nodes};
 
-enum Children<'a, T> {
+pub enum Children<'a, T> {
     Boxes(Vec<BoundingBox<T>>),
     Nodes(Vec<&'a Node<'a, T>>),
 }
 
-struct Node<'a, T> {
+pub struct Node<'a, T> {
     bounds: BoundingBox<T>,
     parent: Option<&'a Node<'a, T>>,
     children: Children<'a, T>,
@@ -18,7 +16,7 @@ trait Searchable<T> {
     fn search(&self) -> Option<T>;
 }
 
-trait TreeState {
+pub trait TreeState {
     fn depth(&self) -> usize;
 
     fn height(&self) -> usize;
@@ -26,6 +24,10 @@ trait TreeState {
     fn is_leaf(&self) -> bool;
 
     fn is_root(&self) -> bool;
+
+    fn num_elements(&self) -> usize;
+
+    fn num_nodes(&self) -> usize;
 }
 
 impl<T> TreeState for Node<'_, T> {
@@ -39,10 +41,14 @@ impl<T> TreeState for Node<'_, T> {
     fn height(&self) -> usize {
         // r trees are always balanced!
         // so you can use the first node on a list always
-
         match &self.children {
             Boxes(_) => 1,
-            Nodes(n) => 1 + n.first().unwrap().height()
+            Nodes(n) => {
+                match n.first() {
+                    None => 0,
+                    Some(x) => 1 + x.height(),
+                }
+            }
         }
     }
 
@@ -55,5 +61,26 @@ impl<T> TreeState for Node<'_, T> {
 
     fn is_root(&self) -> bool {
         self.parent.is_none()
+    }
+
+    fn num_elements(&self) -> usize {
+        match &self.children {
+            Nodes(nodes)
+            => nodes
+                .iter()
+                .map(|node| { node.num_elements() })
+                .sum(),
+            Boxes(boxes) => boxes.len()
+        }
+    }
+
+    fn num_nodes(&self) -> usize {
+        match &self.children {
+            Nodes(nodes)
+            => nodes.iter()
+                .map(|node| { node.num_nodes() })
+                .sum(),
+            Boxes(_) => 1
+        }
     }
 }
