7bec0cc4e3300e5353b327120e71bc2ed3e997c7
hunter <ianrozen@buffalo.edu>
2022-06-29 16:52:02 -0400

    working on r* tree node insertion

--- src/r_nodes/revised_node.rs
+++ src/r_nodes/revised_node.rs
@@ -1,47 +1,65 @@
 use crate::bounding_box::interface::BoundingBox;
-use crate::r_nodes::interface::{Node, NodeManipulation, NodeState};
+use crate::r_nodes::interface::{Children, Node, NodeManipulation, NodeState, NodeTraversal};
+use crate::r_nodes::interface::Parent::Tree;
 
-trait NodeSplits<D> {
-    fn split_needed(&self) -> bool;
+trait NodeHeuristics<D, R> {
+    fn split_needed(&mut self) -> bool;
 
     fn split_node(&self);
+
+    fn lower_check(&self);
+
+    fn choose_subtree(&self) -> Node<'_, D, R>;
 }
 
 
-impl<D, R> NodeSplits<f64> for Node<'_, D, R> {
-    fn split_needed(&self) -> bool {
+impl<D, R> NodeHeuristics<D, R> for Node<'_, D, R> {
+    fn split_needed(&mut self) -> bool {
         todo!()
     }
 
     fn split_node(&self) {
         todo!()
     }
+
+    fn lower_check(&self) { todo!() }
+
+    fn choose_subtree(&self) -> Node<'_, D, R> { todo!() }
 }
 
 
-impl<D, R> NodeManipulation<f64> for Node<'_, D, R> {
-    fn insert(&self, element: BoundingBox<f64>) {
-        if self.is_leaf() {
-            if self.split_needed() {
-                self.split_node();
+impl<D, R> NodeManipulation<D> for Node<'_, D, R> {
+    fn insert(&mut self, element: BoundingBox<D>) {
+        match &mut self.children {
+            Children::Boxes(boxes) => {
+                if self.split_needed() {
+                    self.split_node();
 
-                if self.is_root() {
-                    match self.parent {
-                        None => panic!(),
-                        Some(node) => node.insert(element)
-                    }
-                    todo!()
+                    self.root().insert(element);
+                    return
+                } else {
+                    self.lower_check();
+                    boxes.push(element);
                 }
-            } else {}
+            }
+            Children::Nodes(nodes) => {
+                if self.split_needed() {
+                    self.split_node();
+
+                    self.root().insert(element);
+                } else {
+                    let &mut child = &mut self.choose_subtree();
+                }
+            }
         }
         todo!()
     }
 
-    fn remove(&self, element: BoundingBox<f64>) -> bool {
+    fn remove(&mut self, element: BoundingBox<D>) -> bool {
         todo!()
     }
 
-    fn query(&self, element: BoundingBox<f64>) -> bool {
+    fn query(&self, element: BoundingBox<D>) -> bool {
         todo!()
     }
 }
