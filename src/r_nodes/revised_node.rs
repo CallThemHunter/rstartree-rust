b61d2c5989ab078d967fb99c766e01aac8ed88bf
hunter <ianrozen@buffalo.edu>
2022-07-01 20:04:31 -0400

    trying to rework the insertion process

--- src/r_nodes/revised_node.rs
+++ src/r_nodes/revised_node.rs
@@ -1,59 +1,64 @@
 use crate::bounding_box::interface::BoundingBox;
-use crate::r_nodes::interface::{Children, Node, NodeManipulation, NodeTraversal};
+use crate::r_nodes::interface::{Children, Node, NodeCore, NodeManipulation};
 
-trait NodeHeuristics<'a, D, R> {
+trait NodeHeuristics<D, R> {
     fn split_needed(&self) -> bool;
 
-    fn split_node(&self);
+    fn split_node(&mut self);
 
     fn lower_check(&self);
 
-    fn choose_subtree(&self, nodes: &Vec<&Node<'a, D, R>>) -> Node<'a, D, R>;
+    fn choose_subtree(&self, nodes: &Vec<Node<D, R>>) -> usize;
 }
 
 
-impl<'a, D, R> NodeHeuristics<'a, D, R> for Node<'a, D, R> {
+impl<D, R> NodeHeuristics<D, R> for Node<D, R> {
     fn split_needed(&self) -> bool {
         todo!()
     }
 
-    fn split_node(&self) {
+    fn split_node(&mut self) {
         todo!()
     }
 
     fn lower_check(&self) { todo!() }
 
-    fn choose_subtree(&self, nodes: &Vec<&Node<'a, D, R>>) -> Node<'a, D, R> { todo!() }
+    fn choose_subtree(&self, nodes: &Vec<Node<D, R>>) -> usize { todo!() }
 }
 
 
-impl<'a, D, R> NodeManipulation<'a, D> for Node<'a, D, R> {
-    fn insert(&'a mut self, element: BoundingBox<D>) {
-        match &self.children {
-            Children::Boxes(mut boxes) => {
-                if self.split_needed() {
-                    self.split_node();
-
-                    self.root_mut().insert(element);
-                    return
-                } else {
-                    self.lower_check();
-                    boxes.push(element);
-                    return
-                }
-            }
-            Children::Nodes(mut nodes) => {
-                if self.split_needed() {
+// a bit messy for now
+impl<D, R> NodeManipulation<D, R> for Node<D, R> {
+    fn insert(&mut self, element: BoundingBox<D>) {
+        if self.split_needed() {
+            match &mut self.children.clone().into_inner() {
+                Children::Boxes(_) => {
                     self.split_node();
 
                     self.root().insert(element);
                     return
-                } else {
-                    let child = &mut self.choose_subtree(nodes.as_ref());
+                },
+                Children::Nodes(_) => {
+                    self.split_node();
 
-                    child.insert(element);
+                    self.root().insert(element);
                     return
                 }
+            }
+        } else {
+            match self.children.clone().into_inner() {
+                Children::Boxes(mut boxes) => {
+                    // self.lower_check();
+                    // boxes.push(element);
+                    // return
+                },
+                Children::Nodes(nodes) => {
+                    // let mut child = self.choose_subtree(nodes).remake();
+                    // // let child = self.choose_subtree(nodes);
+                    //
+                    // child.insert(element);
+                    // return
+                },
             }
         }
     }
