6182678f604c036ce817b9c1aa1f7d7bb04655f3
hunter <ianrozen@buffalo.edu>
2022-07-11 22:43:51 -0400

    some implementations of top level tree stuff

--- src/r_nodes/revised_tree.rs
+++ src/r_nodes/revised_tree.rs
@@ -60,19 +60,19 @@ impl<D> NodeCore<D, RRSTree<D>> for RRSTree<D> {
     }
 
     fn children(&self) -> Ref<Children<D, RRSTree<D>>> {
-        todo!()
+        self.root.children()
     }
 
     fn children_mut(&self) -> RefMut<Children<D, RRSTree<D>>> {
-        todo!()
+        self.root.children_mut()
     }
 
-    fn root(&self) -> Ref<Node<D, Self>> {
-        todo!()
+    fn root(&self) -> &Node<D, Self> {
+        &self.root
     }
 
-    fn root_mut(&mut self) -> RefMut<Node<D, Self>> {
-        todo!()
+    fn root_mut(&mut self) -> &mut Node<D, Self> {
+        &mut self.root
     }
 }
 
